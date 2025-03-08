e725eb3758902f2be155cbbc6050fdf92825eba4
Andrew <137727699+andrewpeters0n@users.noreply.github.com>
2025-03-08 21:32:27 +0000

    HW

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,9 @@
+# streamlit_app.py
+
 import streamlit as st
-from datetime import datetime
-st.title('Clock')
-clock = st.empty()
-while True:
-    time = datetime.now().strftime("%H:%M:%S")
-    clock.info('**Current time: ** %s' % (time))
-    if time == '16:09:50':
-        clock.empty()
-        st.warning('Alarm!!')
-        break
+
+conn = st.connection("snowflake")
+df = conn.query("SELECT * FROM mytable;", ttl="10m")
+
+for row in df.itertuples():
+    st.write(f"{row.NAME} has a :{row.PET}:")
